6ce6c0eacfd0b70a9093452626ee24df865fdf09
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2024-06-05 22:27:00 +0700

    Add CSS to components

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -12,12 +12,17 @@ export interface TodosProps {
 
 const Todos: React.FC<TodosProps> = ({ todos }) => {
   return (
-    <div>
+    <div style={styles.container}>
       {todos.map((todo) => {
         return <TodoItem key={todo.id} todo={todo} />
       })}
     </div>
   )
 }
-
+const styles = {
+  container: {
+    width: '40%',
+    margin: '0 auto',
+  },
+}
 export default Todos
