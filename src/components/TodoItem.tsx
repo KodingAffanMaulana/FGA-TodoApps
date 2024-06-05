32391a5dfa6247a9146458ca5fc85cca08d78437
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2024-06-05 22:49:54 +0700

    develop_delete_todo_feature

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -16,6 +16,8 @@ const TodoItem: React.FC<any> = ({ todo, toggleCompleted }) => {
         onChange={() => toggleCompleted(todo.id)}
       />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
+      {/* Tambahkan sebuah button di sini */}
+      <button style={styles.button}>x</button>
     </div>
   )
 };
@@ -24,16 +26,24 @@ const styles = {
   todoItem: {
     border: '2px solid #f4f4f4',
     fontSize: '24px',
-    // Tambahkan styles di bawah ini
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    padding: '0 20px',
   },
-  // Tambahkan styles di bawah ini
   checkbox: {
-    marginRight: '10px',
     height: '18px',
     width: '18px',
+  },
+  button: {
+    backgroundColor: '#BB0000',
+    color: '#fff',
+    height: '30px',
+    width: '30px',
+    borderRadius: '100%',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
   },
 }
 
